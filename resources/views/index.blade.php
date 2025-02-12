ba6447764cd5b05a5b4739fa060c7249cb1d0c56
Grizerl <g397618@gmail.com>
2025-02-12 15:56:38 +0200

    four commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,7 +49,7 @@
                     </figure>
                     <div>
                         <h2 class="section-title">Start to plan your diet today</h2>
-                        <p class="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+                        <p class="section-text max-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
                     </div>
                 </div>
                 <div>
@@ -63,15 +63,83 @@
     </div>
 </section>
 <section>
-    <div>
+    <div class="section-menu">
         <div>
             <div class="container">
-                <div>   
+                <div class="menu-content">   
                     <div>
-                        <h1>Our Menu</h1>
-                        <p>This is a section of your menu. Give your section a brief description</p>
+                        <h1 class="headline-1">Our Menu</h1>
+                        <p class="menu-text text-light max-width">This is a section of your menu. Give your section a brief description</p>
                     </div>
                     <img  src="/assets/images/Image (15).png" alt="Annie-spratt">
+                </div>
+                <div>
+                    <div class="menu-list d-grid">
+                        <div class="menu-item">
+                            <div class="price-container text-right">
+                                <span class="currency">$</span class="price"><span>20</span>      
+                            </div>
+                            <div class="card-content">
+                                <h2 class="card-title max-text">Deep Sea Snow White Cod Fillet</h2>
+                                <p class="card-lead text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="menu-item">
+                            <div class="price-container text-right">
+                                <span class="currency">$</span class="price"><span>22</span>      
+                            </div>
+                            <div class="card-content">
+                                <h2 class="card-title max-text">Steak With Rosemary Butter</h2>
+                                <p class="card-lead text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="menu-item">
+                            <div class="price-container text-right">
+                                <span class="currency">$</span class="price"><span>18</span>      
+                            </div>
+                            <div class="card-content">
+                                <h2 class="card-title max-text">Cucumber Salad</h2>
+                                <p class="card-lead text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="menu-item">
+                            <div class="price-container text-right">
+                                <span class="currency">$</span class="price"><span>90</span>      
+                            </div>
+                            <div class="card-content">
+                                <h2 class="card-title max-text">Natural Wine Pairing</h2>
+                                <p class="card-lead text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="section-excellent-cook">
+        <div>
+            <div class="container">
+                <div class="cook-container d-flex">
+                    <div class="images-block">
+                        <img class="leaf" src="/assets/images/excellent cook/Leaf.png" loading="lazy" alt="Leaf">
+                        <div>
+                            <figure>
+                                <img class="img-wooden" src="/assets/images/excellent cook/brown-wooden-round-plate-with-food 1.png" loading="lazy" alt="Brown wooden">
+                            </figure>
+                            <figure>
+                                <img class="img-chef" src="/assets/images/excellent cook/man-in-black-crew-neck-t-shirt-eating 1.png" loading="lazy" alt="Chef">
+                            </figure>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="section-cook-title">Excellent cook</h1>
+                        <p class="section-cook-text max-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus auctor tincidunt lacus nunc. </p>
+                        <figure class="text-right">
+                            <img class="leaf-section" src="/assets/images/excellent cook/Leaf (1).png" loading="lazy" alt="Lead">
+                        </figure>
+                    </div>
                 </div>
             </div>
         </div>
