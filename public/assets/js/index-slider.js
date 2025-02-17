b7b5bfaf736b9b82f60ca91bad16cddb11d4835f
Grizerl <g397618@gmail.com>
2025-02-17 14:15:59 +0200

    eight commit

--- public/assets/js/index-slider.js
+++ public/assets/js/index-slider.js
@@ -35,3 +35,22 @@ function showSlides(n)
     slides[slideIndex-1].style.display="block"
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerIcon = document.querySelector(".adaptive-icon");
+    const closeButton = document.querySelector(".close-btn");
+    const burgerMenu = document.querySelector(".burger-menu");
+
+    burgerIcon.addEventListener("click", function () {
+        burgerMenu.classList.add("active");
+    });
+
+    closeButton.addEventListener("click", function () {
+        burgerMenu.classList.remove("active");
+    });
+
+    // document.addEventListener("click", function (event) {
+    //     if (!burgerMenu.contains(event.target) && !burgerIcon.contains(event.target)) {
+    //         burgerMenu.classList.remove("active");
+    //     }
+    // });
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,61 @@
                     </div> 
                 </div>
             </header>
+            <div class="burger-menu">
+                <span class="close-btn">&#10006;</span>
+                <div class="burger-container d-flex">
+                    <div class="container-left">
+                        <ul class="left-navbar">
+                            <li class="left-item">
+                                <a class="left-link" href="#">Home</a>
+                            </li>
+                            <li class="left-item">
+                                <a class="left-link" href="#">Menu</a>
+                            </li>
+                            <li class="left-item">
+                                <a class="left-link"  href="#">Blogs</a>
+                            </li>
+                            <li class="left-item">
+                                <a class="left-link" href="#">About</a>
+                            </li>
+                            <li class="left-item">
+                                <a class="left-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="container-right">
+                        <h4 class="right-title">Contact</h4>
+                        <ul>
+                            <li class="right-item">
+                                <a class="right-link" href="tel:+1+86 852 346 000">+1+86 852 346 000</a>
+                            </li>
+                            <li class="right-item">
+                                <a class="right-link" href="mailto:info@foodzero.com">info@foodzero.com</a>
+                            </li>
+                            <li class="right-item">
+                                <a class="right-link" href="https://maps.app.goo.gl/3zqKjMHfo8fH2Cto8">1959 Sepulveda Blvd.</a>
+                            </li>
+                            <li class="right-item">
+                                <a class="right-link" href="https://maps.app.goo.gl/b8h5vpRsZghze8kt5">Culver City, CA, 90230</a>
+                            </li>
+                            <ul class="sc-DdfF d-flex">
+                                <li>
+                                    <a class="media-link" href="#"><img height="25" width="25" src="/assets/images/media-icon/Icon_instagram.svg" alt="Media icon Instagram"></a>
+                                </li>
+                                <li>
+                                    <a class="media-link" href="#"><img height="25" width="25" src="/assets/images/media-icon/Icon_twitter.svg" alt="Media icon Twitter"></a>
+                                </li>
+                                <li>
+                                    <a class="media-link" href="#"><img height="25" width="25" src="/assets/images/media-icon/Icon_facebook.svg" alt="Media icon Facebook"></a>
+                                </li>
+                                <li>
+                                    <a class="media-link" href="#"><img height="25" width="25" src="/assets/images/media-icon/Icon_youtube.svg" alt="Media icon Youtub"></a>
+                                </li>
+                            </ul>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
         @yield('content')
     </div>
